6d619c87e57250eecc325eb20d08592708f04dbd
kepler62f <blue.kepler62f@gmail.com>
2017-06-22 17:17:12 +0800

    First draft

---
+++ script.js
@@ -0,0 +1,49 @@
+ var randomNumber = randomFn(0, 10)
+ var guessedNum = prompt('The first guess: type your number!')
+ var isCorrect = false
+ var gameOver = false
+
+ // fn that generates randomNum
+ // min and max always whole number
+ // random number between min and max (exclusive)
+ function randomFn (min, max) {
+   return Math.floor(Math.random() * (max - min)) + min
+ }
+
+ // this fn. updates h1 text content
+ function updateH1(newH1) {
+   var h1 = document.querySelector('h1')
+   h1.textContent = newH1 }
+
+// fn that checks numbers with the random number
+function askForANumber (guessedNum, randomNumber) {
+  if (guessedNum > randomNumber) {
+    updateH1('lower, randomNumber is ' + randomNumber)
+  } else if (guessedNum < randomNumber) {
+    updateH1('higher, randomNumber is ' + randomNumber)
+  } else if(guessedNum === randomNumber) {
+    updateH1('correct')
+    gameOver = true
+  }
+  checkForGameover()
+}
+
+function newGame() {
+  // setting randomNumber to 5 for easy checking
+  askForANumber( guessedNum, 5 )
+}
+
+function checkForGameover() {
+  if(gameOver) {
+    alert('reveal the restart button now, stop the game')
+  } else {
+    guessedNum = prompt('Type your number again!')
+    // bugs?!
+    askForANumber(guessedNum, 5)
+  }
+}
+
+// bugs?!
+if(! gameOver) {
+  newGame()
+}
